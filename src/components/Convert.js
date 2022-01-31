1977afe4ad7952f95d11fd97b07a0b9cbed0e4c4
Philip <pch4623@gmail.com>
2022-01-31 21:01:56 +0900

    useEffect를 타이머 셋팅 및 입력값 변경 & api 호출 용도로 분리

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,12 +3,26 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
 
+  // 1) text를 debouncedText로 업데이트 하기 위해 0.5초 마다 타이머 셋팅
+  //    & 타이머를 취소하는 함수 리턴 (0.5초 안에 text가 변경되어 rerendering 시점에 동작)
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [text]);
+
+  // 2) debouncedText 업데이트 될 때마다 구글 번역 API 호출
   useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
         params: {
-          q: text,
+          q: debouncedText,
           target: language.value,
           key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
         }
@@ -17,7 +31,7 @@ const Convert = ({ language, text }) => {
     };
 
     doTranslation();
-  }, [language, text]);  // 최초 렌더링 & 상태 변경 될 때마다 리렌더링 되면서 동작
+  }, [language, debouncedText]);  // 최초 렌더링 & 상태 변경 될 때마다 리렌더링 되면서 동작
   
   return (
     <div>
